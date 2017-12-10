8503fc31669c4fa7de14b14322040124491ae31c
ksvitkovsky <ksvitkovsky@yandex.ru>
2017-12-10 15:57:14 +0400

    feature: add main control ui state

--- source/components/main/main.tsx
+++ source/components/main/main.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
 
 import "./main.scss";
 import { Menu, Todos } from "..";
 
+@observer
 export class Main extends React.Component {
+  @observable isMenuOpen: boolean = false;
+
+  @action.bound
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   render() {
     return (
       <div className="main">
-        <div className="main__top-bar" />
-        <Menu className="main__menu" />
+        <div className="main__top-bar">
+          <button className="main__menu-button" onClick={this.toggleMenu}>
+            toggle menu
+          </button>
+        </div>
+        <Menu
+          className={`main__menu ${this.isMenuOpen ? "main__menu--open" : ""}`}
+        />
         <div className="main__menu-overlay" />
         <Todos className="main__todos" />
       </div>
