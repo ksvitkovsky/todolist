41e705655b8ba63964f0a4ee9a3f9506aa1f2cc1
ksvitkovsky <ksvitkovsky@yandex.ru>
2017-12-11 19:10:53 +0400

    feature: add basic menu layout

--- source/components/menu/menu.tsx
+++ source/components/menu/menu.tsx
@@ -8,6 +8,47 @@ interface MenuProps {
 
 export class Menu extends React.Component<MenuProps> {
   render() {
-    return <div className={`${this.props.className} menu`} />;
+    return (
+      <div className={`${this.props.className} menu`}>
+        <ul className="menu__links">
+          <li className="menu__link">
+            <a className="menu__anchor" href="#/unsorted" data-items="0">
+              Unsorted
+            </a>
+          </li>
+          <li className="menu__link">
+            <a
+              className="menu__anchor menu__anchor--active"
+              href="#/today"
+              data-items="3"
+            >
+              Today
+            </a>
+          </li>
+          <li className="menu__link">
+            <a className="menu__anchor" href="#/week" data-items="4">
+              Week
+            </a>
+          </li>
+        </ul>
+        <h4 className="menu__header">Projects</h4>
+        <ul className="menu__links">
+          <li className="menu__link">
+            <a
+              className="menu__anchor"
+              href="#/project/Portfolio"
+              data-items="2"
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="menu__link">
+            <a className="menu__anchor" href="#/project/Sport" data-items="3">
+              Sport
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
